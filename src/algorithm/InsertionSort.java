d300dcc01ea7c3cb84c76c492c899f2f8c3f6d42
cristiano2003 <nguyentrungtruc2003@gmail.com>
2023-07-23 15:25:05 +0700

    delete redundancy

--- src/algorithm/InsertionSort.java
+++ src/algorithm/InsertionSort.java
@@ -10,33 +10,7 @@ public class InsertionSort extends Sort {
     public InsertionSort(double width) {
         super(width);
     }
-    
-//
-//    private int length;
-//
-//    public InsertionSort(int[] arr) {
-//        super(arr);
-//        this.length = arr.length;
-//    }
-//
-//    public void insertionsort(int[] arr) {
-//
-//        int i, key, j;
-//        for (i = 1; i < arr.length; i++) {
-//            key = arr[i];
-//            j = i - 1;
-//
-//            while (j >= 0 && arr[j] > key) {
-//                arr[j + 1] = arr[j];
-//                j = j - 1;
-//            }
-//            arr[j + 1] = key;
-//        }
-//    }
-//    @Override
-//    public void sort(int[] arr) {
-//        insertionsort(arr);
-//    }
+
     private void insertionSort(Unit[] arr) {
         for (int i = 1; i < arr.length; i++) {
             Unit key = arr[i];
